8f724f0318905a89b931adf7d1b0a1216b8e97e0
James Harrington <JRileyHarrington@gmail.com>
2018-07-24 14:01:00 -0400

    assets path update for prod

--- src/js/loader.js
+++ src/js/loader.js
@@ -3,7 +3,7 @@ import * as PIXI from 'pixi.js';
 export default (cb, handleProgress)=>{
     let loader = PIXI.loader;
     loader
-        .add('circle','assets/circle.png')
+        .add('circle','public/assets/circle.png')
         .on("progress", handleProgress)
         .load(cb);
 }
